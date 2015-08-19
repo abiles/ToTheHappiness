f2cc72ab32728076fef6b7ae1deed41804aa39ec
SeongYeon-Kim <pgalpga@hanmail.net>
2015-08-20 01:07:47 +0900

    블럭이 마우스 위치로 갑니다. 벽에 팅깁니다.

---
+++ ToTheHappiness/Assets/Script/Cube.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+    Vector3 startPoint;
+    Vector3 endPoint;
+    public float duration = 100.0f;
+    float zAxis;
+    bool keepMoving = true;
+    bool collisionWithBlcok = false;
+    Ray ray;
+    RaycastHit hit;
+
+    void Start () {
+        startPoint = transform.position;
+        zAxis = transform.position.z;
+        GetEndPoint();
+	}
+
+    void Update()
+    {
+
+        if(collisionWithBlcok)
+        {
+            Vector3 moveVector = endPoint - startPoint;
+            moveVector.x = -moveVector.x;
+            startPoint = transform.position;
+            endPoint = startPoint + moveVector;
+
+            collisionWithBlcok = false;
+        }
+
+        if (keepMoving)
+        {
+            transform.position = Vector3.Lerp(transform.position,
+                                              endPoint,
+                                              1 / (duration * (Vector3.Distance(transform.position, endPoint))));
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Cube"))
+        {
+             keepMoving = false;
+        }
+        else if(collision.gameObject.layer == LayerMask.NameToLayer("Block"))
+        {
+            collisionWithBlcok = true;
+        }
+    }
+
+
+    void GetEndPoint()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            endPoint = hit.point;
+            endPoint.z = zAxis;
+        }
+    }
+
+   
+}
--- ToTheHappiness/Assets/Script/CubePusher.cs
+++ ToTheHappiness/Assets/Script/CubePusher.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class CubePusher : MonoBehaviour {
 
     public GameObject cube;
-
+    
+   
 	
 	// Update is called once per frame
 	void Update () {
@@ -13,14 +14,19 @@ public class CubePusher : MonoBehaviour {
         {
             GameObject newCube = Instantiate(cube) as GameObject;
             newCube.transform.position = transform.position;
-            Vector3 moveDir = newCube.transform.position - Input.mousePosition;
-            moveDir.Normalize();
-            // 이렇게 하면 방향 얻을 수 있고
-            // 이 방향 대로 블럭이 이동하게 만들면 되겠다.
+            //Vector3 moveDir = Input.mousePosition - newCube.transform.position;
+            //moveDir.Normalize();
+            //moveDir.z = 0;
 
+            //Debug.Log(moveDir.x + " " + moveDir.y + " " + moveDir.z);
+            //// 이렇게 하면 방향 얻을 수 있고
+            //// 이 방향 대로 블럭이 이동하게 만들면 되겠다.
+            //int moveSpeed = 10;
+            //Vector3 firstVelocity = new Vector3(moveSpeed, moveSpeed, 0);
+            //Vector3 newVelocity = new Vector3(firstVelocity.x * moveDir.x, firstVelocity.y * moveDir.y, 0);
+            //newCube.GetComponent<Rigidbody>().velocity = newVelocity;
 
-            
-            //newCube.transform.Translate()
+          
         }
 	}
 }
