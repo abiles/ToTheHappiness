de18cfa71065c9bb1387fff7a96f26588fbfc776
SeongYeon-Kim <pgalpga@hanmail.net>
2015-08-19 23:14:15 +0900

    First commit

---
+++ ToTheHappiness/Assets/Script/CubePusher.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubePusher : MonoBehaviour {
+
+    public GameObject cube;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject newCube = Instantiate(cube) as GameObject;
+            newCube.transform.position = transform.position;
+            Vector3 moveDir = newCube.transform.position - Input.mousePosition;
+            moveDir.Normalize();
+            // 이렇게 하면 방향 얻을 수 있고
+            // 이 방향 대로 블럭이 이동하게 만들면 되겠다.
+
+
+            
+            //newCube.transform.Translate()
+        }
+	}
+}
---
+++ ToTheHappiness/Assets/Script/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    int screenWidth = 480;
+    int screenHeight = 800;
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution(Screen.width, Screen.height, true);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
